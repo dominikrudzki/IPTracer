cd7ff280688b8ae75c954bb76731a44e49256030
Dominik Rudzki <dominikrudzki2000@gmail.com>
2023-10-06 19:19:27 +0100

    Add widget extension

---
+++ IPTracerWidget/IPTracerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  IPTracerWidgetBundle.swift
+//  IPTracerWidget
+//
+//  Created by Dominik Rudzki on 06/10/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct IPTracerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        IPTracerWidget()
+    }
+}
