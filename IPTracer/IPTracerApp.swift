4b05bc628b41819f4e3934eb6e59fd504e3d3a12
Dominik Rudzki <dominikrudzki2000@gmail.com>
2023-10-06 19:07:33 +0100

    Initial Commit

---
+++ IPTracer/IPTracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPTracerApp.swift
+//  IPTracer
+//
+//  Created by Dominik Rudzki on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct IPTracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
